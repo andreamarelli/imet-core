19df9462c41e9003a0168f77bf78fbadc68625c3
Andrea Marelli <andrea.marelli.dev@gmail.com>
2021-05-24 18:13:57 +0200

    Adding missing assets

--- src/Assets/js/vue-components.js
+++ src/Assets/js/vue-components.js
@@ -1,3 +1,9 @@
+// Templates
+Vue.component('dopa_radar',                     require('./templates/dopa_radar.vue').default);
+Vue.component('imet_charts',                    require('./templates/imet_charts.vue').default);
+Vue.component('imet_encoders_responsibles',     require('./templates/imet_encoders_responsibles.vue').default);
+Vue.component('imet_radar',                     require('./templates/imet_radar.vue').default);
 
-Vue.component('selector-wdpa',                          require('./inputs/selector-wdpa.vue').default);
-Vue.component('selector-wdpa_multiple',                 require('./inputs/selector-wdpa_multiple.vue').default);
+// Inputs
+Vue.component('selector-wdpa',                  require('./inputs/selector-wdpa.vue').default);
+Vue.component('selector-wdpa_multiple',         require('./inputs/selector-wdpa_multiple.vue').default);
