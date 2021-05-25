6b85c387cbad34556af855e9502ee6aa3095c987
Andrea Marelli <andrea.marelli.dev@gmail.com>
2021-05-25 07:56:07 +0200

    Adding missing assets

--- src/Assets/js/vue-components.js
+++ src/Assets/js/vue-components.js
@@ -5,5 +5,6 @@ Vue.component('imet_encoders_responsibles',     require('./templates/imet_encode
 Vue.component('imet_radar',                     require('./templates/imet_radar.vue').default);
 
 // Inputs
+Vue.component('multiple-files-upload',          require('./inputs/multiple-files-upload.vue').default);
 Vue.component('selector-wdpa',                  require('./inputs/selector-wdpa.vue').default);
 Vue.component('selector-wdpa_multiple',         require('./inputs/selector-wdpa_multiple.vue').default);
