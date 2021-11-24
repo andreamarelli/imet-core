2b9951db222e5a7b932192649763fb81b62691fc
panos <panos.polis@gmail.com>
2021-11-24 12:02:49 +0200

    - refactor custom names form

--- src/Views/scaling_up/components/wdpa_names.blade.php
+++ src/Views/scaling_up/components/wdpa_names.blade.php
@@ -1,37 +1,14 @@
 <?php
-$url = '';
-$method = 'POST';
-$url = route('report_scaling_up', ['items' => $pa_ids]);
-$expanded =  false;
-$title =  trans('imet-core::analysis_report.custom_names');
-
+$accordion_title = $accordion_title ?? trans('imet-core::analysis_report.custom_names');
+$submit_button_label = $submit_button_label ?? @trans('imet-core::analysis_report.apply');
 ?>
 
-<div class="accordion" id="accordion-filters" style="margin-bottom: 40px;">
-    @component('modular-forms::page.accordion', [
-                'accordion_group_id' => 'accordion-filters',
-                'accordion_id' => 'accordion-filters-1',
-                'accordion_title' => $title,
-                'expanded' => $expanded,
-            ])
 
-        @slot('accordion_content')
-            <form class="form-horizontal form-filters" method="{{ $method }}" action="{{ $url }}">
-                {{ csrf_field() }}
-
-                <div class="form-grid">
-                    @foreach($pas as $key => $pa)
-                        {!! \AndreaMarelli\ModularForms\Helpers\Input\Input::label('name', $pa->name) !!}
-                        {!! \AndreaMarelli\ModularForms\Helpers\Input\Input::text($pa->FormID  , $custom_names[$pa->FormID] )!!}
-                    @endforeach
-                </div>
-
-                <div class="text-right">
-                    <button type="submit" class="btn-nav rounded">@lang_u('imet-core::analysis_report.apply')</button>
-                </div>
-
-            </form>
-        @endslot
-
-    @endcomponent
-</div>
+@component('modular-forms::page.filters-accordion', ['accordion_title' => trans('imet-core::analysis_report.custom_names'), 'submit_button_label' => $submit_button_label, 'url'=>route('report_scaling_up', ['items' => $pa_ids]), 'request'=>$request, 'method'=>'POST', 'expanded'=>false])
+    @slot('filter_content')
+        @foreach($pas as $key => $pa)
+            {!! \AndreaMarelli\ModularForms\Helpers\Input\Input::label('name', $pa->name) !!}
+            {!! \AndreaMarelli\ModularForms\Helpers\Input\Input::text($pa->FormID  , $custom_names[$pa->FormID] )!!}
+        @endforeach
+    @endslot
+@endcomponent
