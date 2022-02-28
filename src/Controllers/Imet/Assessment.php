3ade047951700871ed8ea358a1becf2038ad41a2
Andrea Marelli <37180843+andreamarelli@users.noreply.github.com>
2022-02-28 17:50:51 +0100

    Import imet sqlite (#14)
    * ConvertSQLite command signature
    
    * Fixed class name
    
    * Enabled package command
    
    * Import PAs identification
    
    * Converting modules
    
    * Converting context modules
    
    * Converting context modules
    
    * Converting evaluation modules
    
    * Save to file JSON
    
    * Fixed issue with non-WDPA protected areas
    
    * Replace value with key in group fields
    
    * Removed unused v1 to v2 conversion methods
    
    * Fixed issue in IMET v1
    
    * Fixed issues in IMET report
    
    * Fixed issues in IMET report
    
    * Fixed issues
    
    * Import missing form language

--- src/Controllers/Imet/Assessment.php
+++ src/Controllers/Imet/Assessment.php
@@ -173,5 +173,38 @@ trait Assessment{
         ];
     }
 
+
+    public static function score_class($value, $additional_classes=''): string
+    {
+        if($value===null){
+            $class = 'score_no';
+        } elseif($value===0){
+            $class = 'score_danger';
+        } elseif($value<34){
+            $class = 'score_alert';
+        } elseif($value<51){
+            $class = 'score_warning';
+        } else {
+            $class = 'score_success';
+        }
+        return 'class="'.$class.' '.$additional_classes.'"';
+    }
+
+    public static function score_class_threats($value, $additional_classes=''): string
+    {
+        if($value===null){
+            $class = 'score_no';
+        } elseif($value<-51){
+            $class = 'score_danger';
+        } elseif($value<-34){
+            $class = 'score_alert';
+        } elseif($value<-1){
+            $class = 'score_warning';
+        } else {
+            $class = 'score_success';
+        }
+        return 'class="'.$class.' '.$additional_classes.'"';
+    }
+
 }
 
