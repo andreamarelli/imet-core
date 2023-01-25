4293e3a5133f81cb7723912b64df6893edce2dce
Andrea Marelli <andrea.marelli.dev@gmail.com>
2023-01-25 16:09:36 +0100

    OP4 formula reviewed

--- src/Services/Statistics/traits/CustomFunctions/V2/Outputs.php
+++ src/Services/Statistics/traits/CustomFunctions/V2/Outputs.php
@@ -40,42 +40,42 @@ trait Outputs
     {
         $values = AreaDominationMPA::getModule($imet_id);
 
-        $sanctuary_score = $values
-            ->where('group_key', 'group0')
-            ->map(function ($item){
-                return
-                    ($item['Patrol'] +
+        $formula = function($item){
+            return (
+                    $item['Patrol'] +
                     $item['RapidIntervention'] +
                     (int) $item['DetectionRemoteSensing'] +
-                    (int) $item['SpecialMeansRapidIntervention'])
-                    / 8;
+                    (int) $item['SpecialMeansRapidIntervention']
+                )
+                /
+                (
+                    ($item['Patrol']===null ? 0 : 3) +
+                    ($item['RapidIntervention']===null ? 0 : 3) +
+                    ($item['DetectionRemoteSensing']===null ? 0 : 1) +
+                    ($item['SpecialMeansRapidIntervention']===null ? 0 : 1)
+                ) * 100;
+        };
+
+        $sanctuary_score = $values
+            ->where('group_key', 'group0')
+            ->map(function ($item) use ($formula){
+                return $formula($item);
             })
             ->first();
 
         $no_take_score = $values
             ->where('group_key', 'group1')
-            ->map(function ($item){
-                return
-                    ($item['Patrol'] +
-                        $item['RapidIntervention'] +
-                        (int) $item['DetectionRemoteSensing'] +
-                        (int) $item['SpecialMeansRapidIntervention'])
-                    / 8;
+            ->map(function ($item) use ($formula){
+                return $formula($item);
             })->avg();
 
         $buffer_zone_score = $values
             ->whereIn('group_key', ['group2', 'group3'])
-            ->map(function ($item){
-                return
-                    ($item['Patrol'] +
-                        $item['RapidIntervention'] +
-                        (int) $item['DetectionRemoteSensing'] +
-                        (int) $item['SpecialMeansRapidIntervention'])
-                    / 8;
+            ->map(function ($item)use ($formula){
+                return $formula($item);
             })->avg();;
 
-        $average = self::average([$sanctuary_score, $no_take_score, $buffer_zone_score]);
-        $score = $average!==null ? $average / 3 * 100 : null; // TODO: to be verified with Piotr
+        $score = self::average([$sanctuary_score, $no_take_score, $buffer_zone_score]);
 
         return $score!== null ?
             round($score, 2)
