511716ce547d1f94420a2ddf1ef3b6d079c241db
Andrea Marelli <andrea.marelli.dev@gmail.com>
2023-02-16 18:01:15 +0100

    CTX 5.2

--- src/Views/oecm/context/modules/analysis_stakeholder.blade.php
+++ src/Views/oecm/context/modules/analysis_stakeholder.blade.php
@@ -6,9 +6,21 @@
 use \Illuminate\Support\Facades\View;
 use \Wa72\HtmlPageDom\HtmlPageCrawler;
 
-$original_view = View::make('modular-forms::module.edit.body', compact(['collection', 'vue_data', 'definitions']))->render();
+$original_definitions = $definitions;
 
-$dom = HtmlPageCrawler::create('<div>'.$original_view.'</div>');
+// First 3 groups: force "fixed_rows" and "Element" type as disabled
+$definitions['groups'] = array_slice($definitions['groups'], 0, 3);
+$definitions['fixed_rows'] = true;
+$definitions['fields'][0]['type'] = 'disabled';
+$first_groups = View::make('modular-forms::module.edit.type.group_table', compact(['collection', 'vue_data', 'definitions']))->render();
+
+// Other groups: normal
+$definitions = $original_definitions;
+$definitions['groups'] = array_slice($definitions['groups'], 3);
+$other_groups = View::make('modular-forms::module.edit.type.group_table', compact(['collection', 'vue_data', 'definitions']))->render();
+
+// Inject titles
+$dom = HtmlPageCrawler::create('<div>'.$first_groups.$other_groups.'</div>');
 $dom->filter('h5.group_title_'.$definitions['module_key'].'_group0')->before('<h3 style="margin-bottom: 20px;">'.trans('imet-core::oecm_context.AnalysisStakeholder.titles.title0').'</h3>');
 $dom->filter('h5.group_title_'.$definitions['module_key'].'_group3')->before('<h3 style="margin-bottom: 20px;">'.trans('imet-core::oecm_context.AnalysisStakeholder.titles.title1').'</h3>');
 $dom->filter('h5.group_title_'.$definitions['module_key'].'_group7')->before('<h3 style="margin-bottom: 20px;">'.trans('imet-core::oecm_context.AnalysisStakeholder.titles.title2').'</h3>');
