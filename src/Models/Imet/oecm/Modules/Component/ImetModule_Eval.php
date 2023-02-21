4b9e79490844584bb5d6f87732c5abe223930935
Andrea Marelli <andrea.marelli.dev@gmail.com>
2023-02-21 17:55:02 +0100

    Fixed missing declaration

--- src/Models/Imet/oecm/Modules/Component/ImetModule_Eval.php
+++ src/Models/Imet/oecm/Modules/Component/ImetModule_Eval.php
@@ -4,8 +4,9 @@ namespace AndreaMarelli\ImetCore\Models\Imet\oecm\Modules\Component;
 
 
 use AndreaMarelli\ImetCore\Models\Imet\Components\Modules\ImetModule_Eval as BaseImetEvalModule;
+use AndreaMarelli\ImetCore\Models\Imet\oecm\Imet;
 
 class ImetModule_Eval extends BaseImetEvalModule
 {
-
+    protected static $form_class = Imet::class;
 }
--- src/Models/Imet/v2/Modules/Component/ImetModule_Eval.php
+++ src/Models/Imet/v2/Modules/Component/ImetModule_Eval.php
@@ -5,9 +5,12 @@ namespace AndreaMarelli\ImetCore\Models\Imet\v2\Modules\Component;
 
 use AndreaMarelli\ImetCore\Models\Imet\Components\Modules\ImetModule_Eval as BaseImetEvalModule;
 use AndreaMarelli\ImetCore\Models\Imet\Components\Upgrade;
+use AndreaMarelli\ImetCore\Models\Imet\v2\Imet;
 
 class ImetModule_Eval extends BaseImetEvalModule
 {
     use Upgrade;
+
+    protected static $form_class = Imet::class;
 
 }
