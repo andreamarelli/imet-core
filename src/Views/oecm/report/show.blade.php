e1aa213fae1c9973e862063abf1554b774fd7949
panos <panos.polis@gmail.com>
2023-08-02 16:41:18 +0300

    - oecm report changes - minor fix

--- src/Views/oecm/report/show.blade.php
+++ src/Views/oecm/report/show.blade.php
@@ -1,9 +1,12 @@
 <?php
 /** @var \AndreaMarelli\ImetCore\Models\Imet\oecm\Imet $item */
 /** @var array $assessment */
-/** @var array $key_elements */
+/** @var array $key_elements_biodiversity */
+/** @var array $key_elements_ecosystem */
 /** @var array $report */
+/** @var array $wdpa_extent */
 /** @var array $area */
+/** @var bool  $show_api */
 /** @var bool $show_non_wdpa */
 /** @var Array $non_wdpa */
 ?>
@@ -11,8 +14,10 @@
 @include('imet-core::oecm.report.report', [
     'action' => 'show',
     'assessment' => $assessment,
-    'key_elements' => $key_elements,
+    'key_elements_biodiversity' => $key_elements_biodiversity,
+    'key_elements_ecosystem' => $key_elements_ecosystem,
     'report' => $report,
+    'report_schema' => $report_schema,
     'area' => $area,
     'show_non_wdpa' => $show_non_wdpa,
     'non_wdpa' => $non_wdpa,
