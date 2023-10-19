99514c0417497214169c7735945ac99e32f9edaf
Andrea Marelli <andrea.marelli.dev@gmail.com>
2023-10-19 09:45:00 +0200

    Fixed config

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 return [
 
     // Routes' prefixes
-    'web_routes_prefix' => env('WEB_ROUTES_PREFIX', null),
-    'api_routes_prefix' => env('API_ROUTES_PREFIX', null),
+    'web_routes_prefix' => null,
+    'api_routes_prefix' => null,
 
 ];
