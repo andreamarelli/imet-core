62aaa0aeafab3d5074af05906620da2b5e934ab7
Andrea Marelli <andrea.marelli.dev@gmail.com>
2024-05-24 17:50:08 +0200

    Removed unused old dependency methods

--- src/Models/Imet/Components/Modules/ImetModule.php
+++ src/Models/Imet/Components/Modules/ImetModule.php
@@ -105,50 +105,13 @@ class ImetModule extends Module
 
     /**
      * Force locale to IMET language in order to retrieve correct label from lang
-     * @param null $form_id
      */
-    public static function forceLanguage($form_id = null)
+    public static function forceLanguage($form_id = null): void
     {
         if($form_id!==null){
             $FormLang = (static::$form_class)::getLanguage($form_id);
             if($FormLang != App::getLocale()){
                 App::setLocale($FormLang);
-            }
-        }
-    }
-
-    /**
-     * Compare updated records with existing (in DB) and drop from dependant modules
-     * @param $form_id
-     * @param $updatedRecords
-     * @param $dependencyClasses
-     */
-    public static function dropFromDependencies($form_id, $updatedRecords, $dependencyClasses)
-    {
-        $reference_field = (new static())->module_fields[0]['name'];
-        $toBeDropped = static::getModule($form_id)->pluck($reference_field)->diff(
-            collect($updatedRecords)->pluck($reference_field)
-        )->all();
-        $toBeDropped = array_values($toBeDropped);
-
-        foreach ($dependencyClasses as $class){
-            /** @var ImetModule $class */
-            $class::dropListed($form_id, $toBeDropped);
-        }
-    }
-
-
-    /**
-     * Drop all records where first field is listed in the given reference list (use for dependent modules, ex: CTX -> Evaluation)
-     * @param $form_id
-     * @param $reference_list
-     */
-    public static function dropListed($form_id, $reference_list)
-    {
-        $reference_field = (new static())->module_fields[0]['name'];
-        foreach (static::getModuleRecords($form_id)['records'] as $record) {
-            if(in_array($record[$reference_field], $reference_list)){
-                static::destroy($record[(new static())->primaryKey]);
             }
         }
     }
--- src/Models/Imet/v2/Modules/Context/MenacesPressions.php
+++ src/Models/Imet/v2/Modules/Context/MenacesPressions.php
@@ -13,6 +13,13 @@ class MenacesPressions extends Modules\Component\ImetModule
 
     public const REQUIRED_ACCESS_LEVEL = Role::ACCESS_LEVEL_HIGH;
 
+    protected static $DEPENDENCIES = [
+        [Modules\Evaluation\Menaces::class, 'Value'],
+        [Modules\Evaluation\InformationAvailability::class, 'Value'],
+        [Modules\Evaluation\KeyConservationTrend::class, 'Value'],
+        [Modules\Evaluation\ManagementActivities::class, 'Value'],
+    ];
+
     public static $groupByCategory = [
             ['group0'],
             ['group1', 'group2', 'group3', 'group4', 'group5'],
@@ -113,25 +120,7 @@ class MenacesPressions extends Modules\Component\ImetModule
     {
         $vue_data = parent::getVueData($form_id, $collection);
         $vue_data['groupByCategory'] = static::$groupByCategory;
-        $vue_data['warning_on_save'] =  trans('imet-core::v2_context.MenacesPressions.warning_on_save');
         return $vue_data;
-    }
-
-    public static function updateModule(Request $request): array
-    {
-        static::forceLanguage($request->input('form_id'));
-
-        $records = Payload::decode($request->input('records_json'));
-        $form_id = $request->input('form_id');
-
-        static::dropFromDependencies($form_id, $records, [
-            Modules\Evaluation\Menaces::class,
-            Modules\Evaluation\InformationAvailability::class,
-            Modules\Evaluation\KeyConservationTrend::class,
-            Modules\Evaluation\ManagementActivities::class,
-        ]);
-
-        return parent::updateModule($request);
     }
 
     public static function upgradeModule($record, $imet_version = null)
