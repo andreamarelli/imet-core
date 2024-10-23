4d9436efeef8e7352769b45994d15772fd9b4a94
panos <panos.polis@gmail.com>
2024-10-23 09:34:18 +0300

    - analysis refactor

--- src/resources/assets/js/scaling_up_analysis/composables/html_to_image.js
+++ src/resources/assets/js/scaling_up_analysis/composables/html_to_image.js
@@ -39,7 +39,6 @@ export function useHtmlToImage(props, emit) {
         htmlToImage.toPng(element, {
             canvasWidth: element.clientWidth ?? size,
             filter: (node) => {
-                console.log({node});
                 const classNames = node?.className;
                 const id = node?.id;
                 if (typeof classNames !== 'string') {
